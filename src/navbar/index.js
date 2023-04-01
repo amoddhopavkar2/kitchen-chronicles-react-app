4910b1a77a9e59f3947f8a8ad4f4db29d8a54895
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-04-01 14:05:08 -0400

    fix bugs

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -8,7 +8,7 @@ const NavbarComponent = () => {
   return (
     <Navbar collapseOnSelect expand="md" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="/">FoodAlgorithms</Navbar.Brand>
+        <Navbar.Brand href="/">Kitchen Chronicles</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
