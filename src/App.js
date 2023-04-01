e0b3d337eff0c3c4a00e41b93f2bdf8b1d077ddd
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-04-01 13:24:29 -0400

    start user functionality

--- src/App.js
+++ src/App.js
@@ -5,18 +5,37 @@ import Navbar from "./navbar";
 import Search from "./search";
 import MealDetails from "./meal-details";
 import Container from "react-bootstrap/Container";
+import Users from "./users";
+import Login from "./users/login";
+import Register from "./users/register";
+import ProtectedRoute from "./users/protected-route";
+import Profile from "./users/profile";
+import CurrentUser from "./users/current-user";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/meal/details/:fid" element={<MealDetails />} />
-        </Routes>
-      </Container>
+      <CurrentUser>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path="/*" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/meal/details/:fid" element={<MealDetails />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Container>
+      </CurrentUser>
     </BrowserRouter>
   );
 }
