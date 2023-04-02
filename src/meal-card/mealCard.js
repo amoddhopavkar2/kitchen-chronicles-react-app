41704abc84f6d18955be19846a986c4537ba7545
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-04-02 01:25:50 -0400

    more cleanup

--- src/meal-card/mealCard.js
+++ src/meal-card/mealCard.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 const MealCard = ({ meal }) => {
   const navigate = useNavigate();
 
+  const handleClick = () => {
+    navigate(`/meal/details/${meal.idMeal}`);
+  };
+
   return (
-    <Col sm={"6"} md={"6"} lg="3">
-      <Card className={"mt-2 mb-2 h-100"}>
+    <Col sm={6} md={6} lg={3}>
+      <Card className="mt-2 mb-2 h-100">
         <Card.Img variant="top" src={meal.strMealThumb} />
         <Card.Body>
           <Card.Title>{meal.strMeal}</Card.Title>
@@ -18,13 +22,7 @@ const MealCard = ({ meal }) => {
               <span className="badge bg-secondary">{meal.strArea}</span>{" "}
               <span className="badge bg-secondary">{meal.strCategory}</span>
             </h5>
-            <Button
-              onClick={() => {
-                navigate("/meal/details/" + meal.idMeal);
-              }}
-            >
-              View Details
-            </Button>
+            <Button onClick={handleClick}>View Details</Button>
           </Card.Text>
         </Card.Body>
       </Card>
