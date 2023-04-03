9f2ed491c4d7aefd12a494bd278bcae0f5d00c6b
Amod Dhopavkar <dhopavkar.am@northeastern.edu>
2023-04-02 20:45:40 -0400

    add dotenv config

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const USER_API_URL = process.env.REACT_API_BASE;
-const BASE_API_URL = "http://localhost:4000";
+const BASE_API_URL = process.env.REACT_API_BASE;
+const USER_API_URL = BASE_API_URL + "/users";
 
 const api = axios.create({ withCredentials: true });
 
