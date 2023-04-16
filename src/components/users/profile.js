24dbc09883d9b20bcf8e4f69d1455e1d10a67972
Mani <manichandra.teja@gmail.com>
2023-04-15 23:49:01 -0400

    updated code for new user models

--- src/components/users/profile.js
+++ src/components/users/profile.js
@@ -1,26 +1,26 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 import {
   findUserByIdThunk,
   logoutThunk,
   updateProfileThunk,
-} from "./users-thunk";
-import { useNavigate } from "react-router";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import { useEffect, useState } from "react";
-import { Alert, Badge } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { findReviewsByAuthorThunk } from "../reviews/reviews-thunks";
+} from './users-thunk';
+import { useNavigate } from 'react-router';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useEffect, useState } from 'react';
+import { Alert, Badge } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { findReviewsByAuthorThunk } from '../reviews/reviews-thunks';
 import {
   findFollowersThunk,
   findFollowingThunk,
-} from "../follows/follows-thunks";
-import { userLikesFoodThunk } from "../likes/likes-thunks";
-import { parseTime } from "../blog/parseTime";
-import Follows from "../follows/follows";
-import { getBlogsByUserIdThunk } from "../blog/blog-thunks";
+} from '../follows/follows-thunks';
+import { userLikesFoodThunk } from '../likes/likes-thunks';
+import { parseTime } from '../blog/parseTime';
+import Follows from '../follows/follows';
+import { getBlogsByUserIdThunk } from '../blog/blog-thunks';
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -29,6 +29,11 @@ const Profile = () => {
   const [lastName, setLastName] = useState(currentUser.lastName);
   const [email, setEmail] = useState(currentUser.email);
   const [password, setPassword] = useState(currentUser.password);
+
+  const [bio, setBio] = useState(currentUser.bio);
+  const [website, setWebsite] = useState(currentUser.website);
+  const [profilePic, setProfilePic] = useState(currentUser.profilePic);
+
   const [editProfile, setEditProfile] = useState(false);
 
   const [firstNameAlert, setFirstNameAlert] = useState(false);
@@ -38,7 +43,10 @@ const Profile = () => {
   const { reviews } = useSelector((state) => state.reviews);
   const { followers, following } = useSelector((state) => state.follows);
   const [followed, setFollowed] = useState(false);
-  const { blog } = useSelector((state) => state.blog);
+  let { blog } = useSelector((state) => state.blog);
+
+  blog = blog.filter((bg) => bg.author.authorName === currentUser.username);
+
   useEffect(() => {
     dispatch(findUserByIdThunk(currentUser._id));
     dispatch(getBlogsByUserIdThunk(currentUser._id));
@@ -51,20 +59,27 @@ const Profile = () => {
     dispatch(findReviewsByAuthorThunk(currentUser._id));
     dispatch(findFollowersThunk(currentUser._id));
     dispatch(findFollowingThunk(currentUser._id));
-    navigate("/login");
+    navigate('/login');
   };
   const updateUserProfile = () => {
     setFirstNameAlert(false);
     setLastNameAlert(false);
     setPasswordAlert(false);
 
-    if (firstName === "") {
+    if (firstName === '') {
       setFirstNameAlert(true);
-    } else if (lastName === "") {
+    } else if (lastName === '') {
       setLastNameAlert(true);
-    } else if (password === "") {
+    } else if (password === '') {
       setPasswordAlert(true);
     } else {
+      const additionalFields = {};
+      if (currentUser.role === 'BLOGGER') {
+        additionalFields.bio = bio;
+        additionalFields.website = website;
+        additionalFields.profilePic = profilePic;
+      }
+
       dispatch(
         updateProfileThunk({
           ...currentUser,
@@ -72,6 +87,7 @@ const Profile = () => {
           lastName: lastName,
           email: email,
           password: password,
+          ...additionalFields,
         })
       );
       setEditProfile(false);
@@ -85,7 +101,7 @@ const Profile = () => {
       <Alert
         variant="danger"
         onClose={() => setFirstNameAlert(false)}
-        className={firstNameAlert ? "d-block" : "d-none"}
+        className={firstNameAlert ? 'd-block' : 'd-none'}
         dismissible
       >
         <span>The field First Name is required!</span>
@@ -93,7 +109,7 @@ const Profile = () => {
       <Alert
         variant="danger"
         onClose={() => setLastNameAlert(false)}
-        className={lastNameAlert ? "d-block" : "d-none"}
+        className={lastNameAlert ? 'd-block' : 'd-none'}
         dismissible
       >
         <span>The field last name is required!</span>
@@ -101,20 +117,36 @@ const Profile = () => {
       <Alert
         variant="danger"
         onClose={() => setPasswordAlert(false)}
-        className={passwordAlert ? "d-block" : "d-none"}
+        className={passwordAlert ? 'd-block' : 'd-none'}
         dismissible
       >
         <span>The field password is required!</span>
       </Alert>
       {currentUser && (
         <>
+          {currentUser.role === 'BLOGGER' ? (
+            <img
+              src={
+                currentUser.profilePic
+                  ? currentUser.profilePic
+                  : 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'
+              }
+              alt="profileImg"
+              width="148px"
+              height="148px"
+              radius="48px"
+            />
+          ) : (
+            ''
+          )}
+
           <h5>
             <Badge bg="secondary">{currentUser.role}</Badge>
           </h5>
 
           <Form>
             <Form.Group as={Row} className="mb-3" controlId="profileFirstName">
-              <Form.Label column sm="2" className={"text-secondary"}>
+              <Form.Label column sm="2" className={'text-secondary'}>
                 First Name
               </Form.Label>
               <Col sm="10">
@@ -128,7 +160,7 @@ const Profile = () => {
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="profileLastName">
-              <Form.Label column sm="2" className={"text-secondary"}>
+              <Form.Label column sm="2" className={'text-secondary'}>
                 Last Name
               </Form.Label>
               <Col sm="10">
@@ -142,7 +174,7 @@ const Profile = () => {
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="profileUsername">
-              <Form.Label column sm="2" className={"text-secondary"}>
+              <Form.Label column sm="2" className={'text-secondary'}>
                 Username
               </Form.Label>
               <Col sm="10">
@@ -160,7 +192,7 @@ const Profile = () => {
             </Form.Group>
 
             <Form.Group as={Row} className="mb-3" controlId="profileEmail">
-              <Form.Label column sm="2" className={"text-secondary"}>
+              <Form.Label column sm="2" className={'text-secondary'}>
                 Email
               </Form.Label>
               <Col sm="10">
@@ -174,20 +206,108 @@ const Profile = () => {
               </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="profilePassword">
-              <Form.Label column sm="2" className={"text-secondary"}>
+              <Form.Label column sm="2" className={'text-secondary'}>
                 Password
               </Form.Label>
               <Col sm="10">
                 <Form.Control
                   type="password"
                   value={password}
-                  as={"input"}
+                  as={'input'}
                   onChange={(event) => setPassword(event.target.value)}
                   readOnly={!editProfile}
                   plaintext={!editProfile}
                 />
               </Col>
             </Form.Group>
+            {currentUser && currentUser.role === 'BLOGGER' ? (
+              <div>
+                <Form.Group as={Row} className="mb-3" controlId="profileBio">
+                  <Form.Label column sm="2" className={'text-secondary'}>
+                    Bio
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control
+                      type="textarea"
+                      value={bio}
+                      onChange={(event) => setBio(event.target.value)}
+                      readOnly={!editProfile}
+                      plaintext={!editProfile}
+                    />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group
+                  as={Row}
+                  className="mb-3"
+                  controlId="profileWebsite"
+                >
+                  <Form.Label column sm="2" className={'text-secondary'}>
+                    Website
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control
+                      type="text"
+                      value={website}
+                      onChange={(event) => setWebsite(event.target.value)}
+                      readOnly={!editProfile}
+                      plaintext={!editProfile}
+                    />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="profilePic">
+                  <Form.Label column sm="2" className={'text-secondary'}>
+                    Profile Picture
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control
+                      type="text"
+                      value={profilePic}
+                      onChange={(event) => setProfilePic(event.target.value)}
+                      readOnly={!editProfile}
+                      plaintext={!editProfile}
+                    />
+                  </Col>
+                </Form.Group>
+              </div>
+            ) : (
+              ''
+            )}
+
+            {currentUser && currentUser.role === 'ADMIN' ? (
+              <div>
+                <Form.Group as={Row} className="mb-3" controlId="adminType">
+                  <Form.Label column sm="2" className={'text-secondary'}>
+                    Admin Type
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control
+                      type="text"
+                      value={currentUser.type}
+                      readOnly
+                      plaintext
+                    />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="mb-3" controlId="adminFromDate">
+                  <Form.Label column sm="2" className={'text-secondary'}>
+                    Profile Picture
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control
+                      type="text"
+                      value={currentUser.adminFromDate}
+                      readOnly
+                      plaintext
+                    />
+                  </Col>
+                </Form.Group>
+              </div>
+            ) : (
+              ''
+            )}
           </Form>
         </>
       )}
@@ -199,19 +319,19 @@ const Profile = () => {
       )}
 
       <Button
-        className={"ms-5"}
-        onClick={() => navigate("/profile/" + currentUser._id)}
+        className={'ms-5'}
+        onClick={() => navigate('/profile/' + currentUser._id)}
       >
         View Public Profile
       </Button>
       <br />
-      <div className={" mt-3 mb-3"}>
+      <div className={' mt-3 mb-3'}>
         <hr />
 
-        {currentUser && currentUser.role === "BLOGGER" && (
+        {currentUser && currentUser.role === 'BLOGGER' && (
           <>
             <h2>Blogs</h2>
-            <ul className={"list-group mb-3"}>
+            <ul className={'list-group mb-3'}>
               {blog && blog.length === 0 ? (
                 <p>This user haven't written any blog.</p>
               ) : (
@@ -219,8 +339,8 @@ const Profile = () => {
                   .filter((bg) => bg.author.authorName === currentUser.username)
                   .map((b) => (
                     <li
-                      className={"list-group-item"}
-                      onClick={() => navigate("/blog/details/" + b._id)}
+                      className={'list-group-item'}
+                      onClick={() => navigate('/blog/details/' + b._id)}
                       key={b._id}
                     >
                       <h5>{b.title}</h5>
@@ -231,7 +351,7 @@ const Profile = () => {
                         }}
                         className="red"
                       ></i>
-                      <div className={"text-secondary"}>
+                      <div className={'text-secondary'}>
                         <span>By: {b.author.authorName}</span>
                         <i className="bi bi-dot"></i>
                         <span>{parseTime(b.time)}</span>
@@ -246,18 +366,18 @@ const Profile = () => {
         <Follows uid={currentUser._id} />
 
         <h2>Comments</h2>
-        <ul className={"list-group mb-3"}>
+        <ul className={'list-group mb-3'}>
           {currentUser && reviews && reviews.length === 0 ? (
             <p>This user haven't posted any comments yet.</p>
           ) : (
             reviews.map((u) => (
               <li
-                className={"list-group-item"}
+                className={'list-group-item'}
                 onClick={() => navigate(`/meal/details/${u.idMeal}`)}
               >
-                <span className={"fw-bold"}>
+                <span className={'fw-bold'}>
                   <Link
-                    className={"text-black"}
+                    className={'text-black'}
                     to={`/profile/${u.author._id}`}
                   >
                     {u.author.username}
@@ -275,7 +395,7 @@ const Profile = () => {
       </div>
       <hr />
 
-      <Button className={"btn-danger mt-3"} onClick={handleLogoutBtn}>
+      <Button className={'btn-danger mt-3'} onClick={handleLogoutBtn}>
         Logout
       </Button>
     </>
